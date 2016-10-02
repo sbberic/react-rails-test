86f6be4a8a9d158711c293804cf8bca67eefd28e
Eric Han <eric.han@overlaygaming.com>
2016-10-02 15:20:37 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReactRailsTest::Application.config.secret_token = 'd162a04b44e7909bf18bd236de29c657e8106a7f186608e3b1b06a3cb5f81c4f9fac26a79f28852316ba7ef9b0bc6cab60027c1f640bbc0b06b608c3667406cf'
